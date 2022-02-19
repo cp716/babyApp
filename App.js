990e0304b6a5fb2707f7f34341aecf25b653c61f
cp716 <98893950+cp716@users.noreply.github.com>
2022-02-19 23:10:39 +0900

    Update App.js
    testCommit

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>テストテストテスト</Text>
+      <Text>テストgitテスト</Text>
       <Text>テスト</Text>
       <StatusBar style="auto" />
     </View>
