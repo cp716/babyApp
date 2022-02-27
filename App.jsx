ea543583f35fb66c19deb67cb989e7b8fd7fae22
cp716 <98893950+cp716@users.noreply.github.com>
2022-02-27 11:55:48 +0900

    Create LoginScreen and SignUpScreen

--- App.jsx
+++ App.jsx
@@ -4,12 +4,16 @@ import BabyTodayScreen from './src/screens/BabyTodayScreen';
 import DetailScreen from './src/screens/DetailScreen';
 import EditScreen from './src/screens/EditScreen';
 import CreateScreen from './src/screens/CreateScreen';
+import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   return (
     //<BabyTodayScreen />
     //<DetailScreen />
-    <EditScreen />
+    //<EditScreen />
     //<CreateScreen />
+    //<LogInScreen />
+    <SignUpScreen />
   );
 }
