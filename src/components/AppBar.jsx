58f79b56537d8e259b68200cb68b079f278e39c6
cp716 <98893950+cp716@users.noreply.github.com>
2022-03-12 23:02:28 +0900

    堀古さんとの競合修正Appber.jsx

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -6,11 +6,7 @@ export default function AppBar() {
         <View style={styles.appber}>
             <View style={styles.appberInner}>
                 <Text style={styles.appberTitle}>Baby App</Text>
-<<<<<<< HEAD
-                <Text style={styles.appberRight}>アウト</Text>
-=======
-                <Text style={styles.appberRight}>ログアウトテスト</Text>
->>>>>>> ff9da65abb0152bf17e50d4a4e7b1c9ce5fba4c6
+                <Text style={styles.appberRight}>ログアウト</Text>
             </View>
         </View>
     )
