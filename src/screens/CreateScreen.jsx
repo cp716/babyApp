2cb681f6985e8fd08550c4c1887441bad60bd5d9
cp716 <98893950+cp716@users.noreply.github.com>
2022-03-23 23:30:44 +0900

    datebase touroku

--- src/screens/CreateScreen.jsx
+++ src/screens/CreateScreen.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 
 import CircleButton from '../components/CircleButton';
 import KeyboardSafeView from '../components/KeyboardSafeView';
 
+import firebase from 'firebase';
+
 export default function CreateScreen(props) {
     const { navigation } = props;
+    const [bodyText, setBodyText] = useState('');
+
+    function handlePress() {
+        const db = firebase.firestore();
+        const { currentUser } = firebase.auth();
+        const ref = db.collection(`users/${currentUser.uid}/memos`);
+        ref.add({
+            bodyText,
+            updatedAt: new Date(),
+        })
+            .then((docRef) => {
+                console.log('書き込みました', docRef.id);
+                navigation.goBack();
+            })
+            .catch((error) => {
+                console.log('失敗しました', error);
+            });
+    }
+
     return (
         <KeyboardSafeView style={styles.container}>
             <View style={styles.inputContainer}>
-                <TextInput value="" multiline style={styles.input} />
+                <TextInput
+                    value={bodyText}
+                    multiline
+                    style={styles.input}
+                    onChangeText={(text) => { setBodyText(text); }}
+                    autoFocus
+                />
             </View>
             <CircleButton
                 name="check"
-                onPress={() => { navigation.goBack(); }}
+                onPress={handlePress}
             />
         </KeyboardSafeView>
     );
