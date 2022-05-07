3074c0d291428eb6343ad1daa830579fb201c25f
gon910 <jntp.R-nkym910@outlook.jp>
2022-05-07 11:06:41 +0900

    Datetime

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function Date() {
+export default function Datetime() {
     return (
         <View style={styles.date}>
             <Text style={styles.dateText}>
