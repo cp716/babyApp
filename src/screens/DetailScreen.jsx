f672e4061bd84fa3c1cfceaa66cac67a11450d97
orang <orange100perjuice@gmail.com>
2022-07-02 11:47:54 +0900

    デザイン調整

--- src/screens/DetailScreen.jsx
+++ src/screens/DetailScreen.jsx
@@ -1,86 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { shape, string } from 'prop-types'
-import { View, ScrollView, Text, StyleSheet, KeyboardSafeView, TextInput } from 'react-native';
-import firebase from 'firebase';
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
 
 import CircleButton from '../components/CircleButton';
 
-const date = new Date();
-const year = date.getFullYear();
-const month = date.getMonth() + 1;
-const day = date.getDate();
-
 export default function DetailScreen(props) {
     const { navigation, route } = props;
-    const { id, bodyText, timeLeft, timeRight, updatedAt } = route.params;
+    const { id } = route.params;
     console.log(id);
-    const [, setMemo] = useState(null);
-
-    const [body, setBodyText] = useState(bodyText);
-    const [left, setTimeLeft] = useState(("00" + timeLeft ).slice(-2));
-    const [right, setTimeRight] = useState(("00" + timeRight ).slice(-2));
-    const [datedAt, setUpdatedAt] = useState(updatedAt);
-
-    useEffect(() => {
-        const { currentUser } = firebase.auth();
-        let unsubscribe = () => {};
-        if (currentUser) {
-            const db = firebase.firestore();
-            const ref = db.collection(`users/${currentUser.uid}/${year}/${month}/${day}`).doc(id);
-            unsubscribe = ref.onSnapshot((doc) => {
-                console.log(doc.id, doc.data());
-                const data = doc.data();
-                setMemo({
-                    id: doc.id,
-                    timeLeft: data.timeLeft,
-                    timeRight: data.timeRight,
-                    bodyText: data.bodyText,
-                    updatedAt: data.updatedAt.toDate(),
-                });
-            });
-        }
-        return unsubscribe;
-    }, []);
-
-
     return (
-        
         <View style={styles.container}>
-            
+            <View style={styles.Record}>
+                <View>
+                    <Text>
+                    一日の記録
+                    </Text>
+                </View>
+            </View>
             <View style={styles.Detail}>
-                <TextInput
-                        //keyboardType="number-pad"
-                        value={body}
-                        multiline
-                        style={styles.input}
-                        onChangeText={(text) => { setBodyText(text); }}
-                        autoFocus
-                />
-                <TextInput
-                        keyboardType="number-pad"
-                        value={left }
-                        multiline
-                        style={styles.input}
-                        onChangeText={(text) => { setTimeLeft(text); }}
-                />
-                <TextInput
-                        keyboardType="number-pad"
-                        value={right}
-                        multiline
-                        style={styles.input}
-                        onChangeText={(text) => { setTimeRight(text); }}
-                />
-                <TextInput
-                        keyboardType="number-pad"
-                        value={datedAt}
-                        multiline
-                        style={styles.input}
-                        onChangeText={(text) => { setUpdatedAt(text); }}
-                />
+                <View>
+                    <Text style={styles.DetailFont}>
+                    2022年1月1日　12：00
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.Test}>
+                <View style={styles.Test1}>
+                    <View>
+                        <Text style={styles.Text1}>
+                        {'母乳\n左\n右'}
+                        </Text>
+                    </View>
+                </View>
+                 <View style={styles.Test1}>
+                    <View>
+                        <Text style={styles.Text2}>
+                        哺乳瓶
+                        </Text>
+                    </View>
+                </View>
+             </View>
+            <View style={styles.Memo}>
+                <ScrollView>
+                    <Text>
+                    メモ
+                    </Text>
+                </ScrollView>
             </View>
             <CircleButton
-                style={{ top: 60, bottom: 'auto'}}
+                style={{ top: 700, left: 315}}
                 name="check"
+                onPress={() => { navigation.navigate('Edit') }}
+            />
+              <CircleButton
+                style={{ top: 700, left: 10}}
+                name="cancel"
                 onPress={() => { navigation.navigate('Edit') }}
             />
         </View>
@@ -96,13 +70,85 @@ DetailScreen.propTypes = {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#ffffff',
+        backgroundColor: 'white',
+    },
+    Record: {
+        backgroundColor: '#ffae87',
+        //justifyContent: 'center',
+        alignItems:'center',
+        paddingtop: 0,
+        paddingVertical: 50,
+        paddingHorizontal: 19,
     },
     Detail: {
-        backgroundColor: 'red',
+        backgroundColor: 'white',
+        //justifyContent: 'center',
+        //paddingVertical: 50,
+        //paddingHorizontal: 19,
+        alignSelf: "center",
+        borderColor: "#ffae87",
+        borderWidth: 5,
+        borderRadius: 15,
+        marginTop: 10,
+        width: '80%',
+        height: '5%',
+    },
+    DetailFont: {
+        //paddingVertical: 10,
+        //paddingHorizontal: 19,
+        alignSelf: "center",
+    },
+    Test: {
+        backgroundColor: 'white',
+        //justifyContent: 'center',
+        //alignItems:'center',
+        paddingVertical: 50,
+        paddingHorizontal: 19,
+        //alignSelf: "center",
+        marginTop: 0,
+        flex: 1,
+        width: '100%',
+        flexDirection: 'row',
+        //justifyContent: 'center',
+        justifyContent: 'space-around',
+    },
+    Test1: {
+        backgroundColor: 'white',
+        //justifyContent: 'center',
+        alignItems:'center',
+        //paddingVertical: 50,
+        //paddingHorizontal: 50,
+        //alignSelf: "center",
+        borderColor: "#ffae87",
+        borderWidth: 5,
+        borderRadius: 15,
+        marginTop: 0,
+        height: "30%",
+        width: "45%",
+    },
+    Text1: {
+        //textAlign:'center',
+        //paddingBottom: 'auto',
+        //paddingTop: 'auto',
+        //marginright: auto,
+        //width: "40%",
+        //flexGrow: 1,
+    },
+    Text2: {
+       // paddingBottom: 50,
+        paddingTop: 20,
+    },
+    Memo: {
+        backgroundColor: 'white',
         //justifyContent: 'center',
         alignItems:'center',
         paddingVertical: 50,
-        paddingHorizontal: 19,
+        paddingHorizontal: 100,
+        alignSelf: "center",
+        borderColor: "#ffae87",
+        borderWidth: 5,
+        borderRadius: 15,
+        marginTop: 0,
+        
     },
 });
